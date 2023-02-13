9113a545f4ce3b7e430ecc77b77e9297ec87ac81
afk <alankarnil@gmail.com>
2023-02-13 17:53:22 +0000

    added more timer tests

--- tests/timer_test.cpp
+++ tests/timer_test.cpp
@@ -39,3 +39,39 @@ TEST_F(CPUtest, increment_div_timer)
 
 	ASSERT_EQ(bus.getMemory(DIV, debug), 0xAC);
 }
+
+TEST_F(CPUtest, increment_timers_tima_256KHz)
+{
+	bus.setMemory(0xFF07, (0b11111100 | 0b00000001), debug);
+
+	// in 64 m cycles, the div timer increments by 1
+	for (int i = 0; i < 3; i++)
+		bus.cpu.updateTimers(4);
+
+	ASSERT_EQ(bus.getMemory(TIMA, debug), 0x00);
+
+	bus.cpu.updateTimers(4);
+
+	ASSERT_EQ(bus.getMemory(TIMA, debug), 0x01);
+}
+
+TEST_F(CPUtest, increment_timers_tima_256KHz_to_overflow)
+{
+	bus.setMemory(0xFF07, (0b11111100 | 0b00000001), debug);
+
+	// in 64 m cycles, the div timer increments by 1
+	for (int i = 0; i < (4*0xFF) - 1; i++)
+		bus.cpu.updateTimers(4);
+
+	ASSERT_EQ(bus.getMemory(TIMA, debug), 0xFE);
+
+	bus.cpu.updateTimers(4);
+
+	ASSERT_EQ(bus.getMemory(TIMA, debug), 0xFF);
+
+	for (int i = 0; i < 4; i++)
+		bus.cpu.updateTimers(4);
+
+	ASSERT_EQ(bus.getMemory(TIMA, debug), 0x00);
+
+}
