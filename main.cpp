e1ce13d33cf88bb910c648d02cb3f816cea0a679
afk <alankarnil@gmail.com>
2023-05-06 19:05:22 +0100

    cmd save hint

--- main.cpp
+++ main.cpp
@@ -43,16 +43,17 @@
 
 int main(int argc, char* argv[])
 {
-    /*
-    if (argc == 1)
+    
+    /*if (argc == 1)
     {
         std::cout << "Usage:\nemuboy.exe <PATH TO ROM> <PATH TO BOOTROM (Optional)>\n\n";
         std::cout << "Controls:\nArrow Keys = D-Pad\nB, N = Start, Select\nA, S = a, b\n\n";
         std::cout << "Extra:\n2, 3 = VRAM Viewer, BG Map Viewer\n[, ] = change address range of BG MAP viewer\n8 = change colour palette\nP = pause\nSPACE = toggle fast forward\n+, - = scaling\n";
+        std::cout << "\nSaves are stored in the same directory as the the ROM with a .sav extension\n";
 
         return 0;
-    }
-    */
+    }*/
+    
     std::string rom_path = ((argv[1] != NULL) ? argv[1] : "..\\..\\..\\roms\\blargg\\full.gb");
     std::string bootrom_path = ((argv[2] != NULL) ? argv[2] : "..\\..\\..\\bios\\bios.bin");
 
@@ -63,9 +64,9 @@ int main(int argc, char* argv[])
     //rom_path = "D:\\afk\\Downloads\\bgbw64\\bgbtest.gb";
     //rom_path = "..\\..\\..\\roms\\metasprites.gb";
     //rom_path = "..\\..\\..\\roms\\empty.gb";
-    //rom_path = "..\\..\\..\\roms\\Super Mario Land.gb";
+    rom_path = "..\\..\\..\\roms\\Super Mario Land.gb";
     //rom_path = "..\\..\\..\\roms\\Prince of Persia.gb";
-    rom_path = "..\\..\\..\\roms\\DR.MARIO.gb";
+    //rom_path = "..\\..\\..\\roms\\DR.MARIO.gb";
     //rom_path = "C:\\Users\\afk\\Downloads\\Nintendo - Game Boy\\Metroid II - Return of Samus (World).gb";
     //rom_path = "..\\..\\..\\roms\\POKEMON YELLOW.gbc";
     //rom_path = "C:\\Users\\afk\\Downloads\\Nintendo - Game Boy\\Donkey Kong (World) (Rev A) (SGB Enhanced).gb";
